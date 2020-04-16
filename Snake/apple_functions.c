d4f8420340e9320ff5c3f4cd452b5b039699e782
toxicmouse7 <utiha@sasuke.ru>
2020-04-17 00:34:11 +0500

    FINAL FCN VARIANT

--- Snake/apple_functions.c
+++ Snake/apple_functions.c
@@ -8,11 +8,19 @@ void Add_Random_Apple(Apple* apple)
 
 void Apple_Collision(Apple* apple, Snake* snake)
 {
-	while (apple->x == snake->x && apple->y == snake->y)
+	Snake* temp = snake;
+	while (temp != NULL)
 	{
-		Add_Random_Apple(apple);
+		if (temp->x == apple->x && temp->y == apple->y)
+		{
+			Add_Random_Apple(apple);
+			temp = snake;
+		}
+		temp = temp->next;
 	}
 }
+
+//void Apple_Collision_Check(Apple* apple, Snake* snake);
 
 void Output_Apple(Apple* apple)
 {

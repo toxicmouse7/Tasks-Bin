2905192994ce1e2b9a0644c215e3cddf0b39712a
toxicmouse7 <utiha@sasuke.ru>
2020-05-24 16:26:51 +0500

    func deletebysurname

--- LAB_4/all_about_names.h
+++ LAB_4/all_about_names.h
@@ -1,13 +1,20 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <Windows.h>
 
 const char* names[] = { "Alexey", "Smith", "Jhonson", "Williams", "Jones", "Brown", "Davis", "Miller", "Willson", "Moore", "Taylor" };
 const char* surnames[] = { "Adams","Baker", "Brook", "Carter", "Collins", "Davies", "Grant", "Harris", "Hill", "Hughes", "Lee" };
-const char* groups[] = {"group1"};
-#define MAX_OF_NAMES 12
-#define MAX_OF_SURNAMES 12
+const char* groups[] = {"group1", "group2","group3" ,"group4" ,"group5" ,"group6" ,"group7" ,"group8" ,"group9" ,"group10" ,"group11" };
+#define MAX_OF_NAMES _countof(names)
+#define MAX_OF_SURNAMES _countof(surnames)
+#define MAX_OF_GROUPS _countof(groups)
 
 struct Student
 {
+	BOOL IsInitilized;
 	char* name;
 	char* surname;
 	int age;
